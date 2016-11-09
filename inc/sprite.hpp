2a8d2880e8c6e7d970345520c3f2e286afb98d4b
liesard <makarafreak@gmail.com>
2016-11-09 10:26:37 +0100

    Added Scenes to organise sprites

--- inc/sprite.hpp
+++ inc/sprite.hpp
@@ -10,18 +10,24 @@
 #include "shader.hpp"
 
 class Sprite {
-private:
-
 public:
+    enum Scene {
+        BACKGROUND = 0,
+        GAMEPLAY,
+        FOREGROUND
+    };
     std::array<GLfloat, 16> vertices;
     std::array<u_int8_t, 6> elements;
     GLuint vbo, ebo, vao, texture;
     GLint vao_index, tex_index;
+    Scene scene;
     void sync(Entity &e);
     void updateTexture(Shader &s);
 
-    Sprite(Shader &s);
+    Sprite(Entity &e, Shader &s, Scene sc);
     Sprite(Entity &e, Shader &s);
+    Sprite(Shader &s, Scene sc);
+    Sprite(Shader &s);
     ~Sprite();
 };
 
--- src/sprite.cpp
+++ src/sprite.cpp
@@ -19,32 +19,13 @@ void Sprite::updateTexture(Shader &s) {
                           (void*)(2 * sizeof(float)));
 }
 
-Sprite::Sprite(Shader &s) {
-    glGenBuffers(1, &vbo);
-    glGenBuffers(1, &ebo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-
-    vao_index = glGetAttribLocation(s, "position");
-    glVertexAttribPointer(vao_index, 2, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(vao_index);
-}
-
-Sprite::Sprite(Entity &e, Shader &s) {
+Sprite::Sprite(Entity &e, Shader &s, Scene sc) : scene {sc} {
     vertices = e.vertices;
 
     elements = {{
         0, 1, 2,
         2, 3, 0
     }};
-
-    glGenVertexArrays(1, &vao);
-    glGenBuffers(1, &vbo);
-    glGenBuffers(1, &ebo);
-
-    glBindVertexArray(vao);
     
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float),
@@ -53,10 +34,8 @@ Sprite::Sprite(Entity &e, Shader &s) {
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, elements.size(),
                  elements.data(), GL_STATIC_DRAW);
 
-    vao_index = glGetAttribLocation(s, "position");
     glVertexAttribPointer(vao_index, 2, GL_FLOAT, GL_FALSE,
                           4 * sizeof(float), 0);
-    glEnableVertexAttribArray(vao_index);
 
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
@@ -75,6 +54,69 @@ Sprite::Sprite(Entity &e, Shader &s) {
 
     glGenerateMipmap(GL_TEXTURE_2D);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+}
+
+Sprite::Sprite(Shader &s, Scene sc) : scene {sc} {
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    vao_index = glGetAttribLocation(s, "position");
+    glVertexAttribPointer(vao_index, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(vao_index);
+}
+
+Sprite::Sprite(Entity &e, Shader &s) : Sprite(s) {
+    vertices = e.vertices;
+
+    elements = {{
+        0, 1, 2,
+        2, 3, 0
+    }};
+    
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float),
+                 vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, elements.size(),
+                 elements.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(vao_index, 2, GL_FLOAT, GL_FALSE,
+                          4 * sizeof(float), 0);
+
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+    int width, height, img_comp;
+    unsigned char *image = stbi_load("res/spritesheet.png", &width, &height,
+                                     &img_comp, 4);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
+                 GL_UNSIGNED_BYTE, image);
+    tex_index = glGetAttribLocation(s, "tex_coord");
+    glEnableVertexAttribArray(tex_index);
+    glVertexAttribPointer(tex_index, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float),
+                          (void*)(2 * sizeof(float)));
+
+    glGenerateMipmap(GL_TEXTURE_2D);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+}
+
+Sprite::Sprite(Shader &s) {
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    vao_index = glGetAttribLocation(s, "position");
+    glVertexAttribPointer(vao_index, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(vao_index);
 }
 
 Sprite::~Sprite() {
